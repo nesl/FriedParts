0f923ef3a584631e64e047b10f3e0cdd1ece1a69
kg4lod <jf@atlalabs.com>
2011-02-14 02:25:23 -0800

    Updated the cleanup script to further polish the FriedParts directory to make commits easy to see (make sure I don't miss any changed files)

--- Cleanup.bat
+++ Cleanup.bat
@@ -1,4 +1,7 @@
 @ECHO OFF
 echo This script will delete all Temporary Files left behind by unclean Visual Studio shutdowns.
+echo It will also cleanup the windows system droppings (like thumbs.db)
 pause
 del /S *.TMP
+del /S /ASH Thumbs.db
+del /S *.bak

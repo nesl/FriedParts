fc1f939b9479931e1da2209fc718fb1222d20b90
kg4lod <jf@atlalabs.com>
2011-03-23 18:22:52 -0700

    IPO

---
+++ App_Code/sys/env/sysSecret.vb
@@ -0,0 +1,18 @@
+Imports Microsoft.VisualBasic
+Imports System
+
+''' <summary>
+''' DO NOT COMMIT OR RELEASE THIS CLASS! THIS INFORMATION IS STRICTLY CONFIDENTIAL!!!
+''' This module holds all of the apps constant, but secret data.
+''' </summary>
+''' <remarks>Do not release. This file is protected by a .gitignore</remarks>
+Public Module sysSecret
+    ''' <summary>
+    ''' Dropbox Web App Production Keys
+    ''' </summary>
+    ''' <remarks></remarks>
+    Public Const DropboxAppKey As String = ""
+    Public Const DropboxAppSecret As String = ""
+    Public Const OctopartAppKey As String = ""
+    Public Const FriedPartsAppKey As String = ""
+End Module
